98013b8a31b397db3027505215e317c6c8859fa7
mpepper <markpepper@gambitresearch.com>
2024-12-09 09:58:51 +0000

    Day 7 and Day8.
    Did day 8 very quickly. Day 7 was a mess, so I did it in python in the end

--- 2024/Day7/Day7.py
+++ 2024/Day7/Day7.py
@@ -5,7 +5,6 @@ filepath = "./Day7Input.txt"
 total_sum = 0
 with open(filepath, 'r') as file:
     for line in file:
-        print(line)
         aim = int(line.split(':')[0])
         inputs = [int(i) for i in line.split(' ')[1:]]
         
@@ -13,13 +12,13 @@ with open(filepath, 'r') as file:
         for i in range(3 ** (len(inputs) - 1)):
             sum = inputs[0]
             for j in range(1,len(inputs)):
-                base_three = int((np.floor(i / (3 ** j)) % 3))
+                base_three = int((np.floor(i / (3 ** (j-1))) % 3))
                 if (base_three == 0):
                     sum += inputs[j]
                 elif (base_three == 1):
                     sum *= inputs[j]
                 else:
-                    sum = (sum * (10 ** (int(np.floor(np.log10(inputs[j])) + 1))))
+                    sum = (sum * (10 ** (int(np.floor(np.log10(inputs[j])) + 1)))) + inputs[j]
 
             if sum == aim:
                 total_sum += aim
